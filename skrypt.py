5594092b78cd1e9a4088107b80e253bb6856baf5
Mikzzer <114287425+Mikzzer@users.noreply.github.com>
2023-03-25 17:51:10 +0100

    Update skrypt.py

--- skrypt.py
+++ skrypt.py
@@ -5,7 +5,7 @@ to_deploy_path = "/home/vagrant/pliczki/workspace/Diploy_proba/war/SampleWebApp.
 
 deploy_path = "/opt/tomcat/webapps"
 
-os.system(f"cp {to_deploy_path} {deploy_path} ")
+os.system(f"sudo cp {to_deploy_path} {deploy_path} ")
 
 
 
