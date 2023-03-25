35a6fcbc486bd97c4ada9d740c2ed3216a700019
Mikzzer <114287425+Mikzzer@users.noreply.github.com>
2023-03-25 17:45:47 +0100

    Update skrypt.py

--- skrypt.py
+++ skrypt.py
@@ -1,15 +1,9 @@
 import git
 import os
 
-repo_path = "/home/vagrant/repo"
-
-to_deploy_path = "/home/vagrant/repo/war/SampleWebApp.war"
+to_deploy_path = "/home/vagrant/pliczki/workspace/Diploy_proba/war/SampleWebApp.war"
 
 deploy_path = "/opt/tomcat/webapps"
-
-
-repo_url = "https://github.com/Mikzzer/Tomcat_deploy.git"
-git.Repo.clone_from(repo_url, repo_path)
 
 os.system(f"cp {to_deploy_path} {deploy_path} ")
 
